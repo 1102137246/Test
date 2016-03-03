1ebf972b35e144e38eab5f504aa80355a58992d4
1102137218 <1102137218@gm.kuas.edu.tw>
2016-03-03 15:48:54 +0800

    add yuling class

---
+++ 0303test/YuLingChen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0303test
+{
+    class YuLingChen
+    {
+        public static void Yuling()
+        {
+            System.Console.WriteLine("玉苓887");
+        }
+    }
+}
